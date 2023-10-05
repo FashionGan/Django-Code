a214865796fbf07153209e51dc458bfd76bed4aa
Fusion-Pie <piyushhatewar@gmail.com>
2023-10-05 19:31:47 +0530

    basic code

---
+++ GAN_APP_1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GanApp1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'GAN_APP_1'
---
+++ GAN_APP_1/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from pexels_api import API
+from django.conf import settings
+import requests
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+
+def getPhotos(request):
+    api  = API(settings.PEXELS_API_KEY)
+    print(api)
+
+    endpoint = 'https://api.pexels.com/v1/curated'
+    
+    images = api.search(endpoint, results_per_page=15)['photos']
+    print(images)
+
+    return render(request, 'index.html', {'image': images})
