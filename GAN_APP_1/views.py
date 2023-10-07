126337a2b592321c53d01062de0b8ec8caaee862
Fusion-Pie <piyushhatewar@gmail.com>
2023-10-07 20:11:16 +0530

    Added Generator code and logic for displaying images in the frontend without saving

--- GAN_APP_1/views.py
+++ GAN_APP_1/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from pexels_api import API
 from django.conf import settings
-import requests
+import tensorflow as tf
+from tensorflow import keras
+import base64
+from io import BytesIO
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+    
+
+def generate_images(request):
+    generator = keras.models.load_model("generator_model.h5")
+
+    batch_size = 8
+    latent_dim = 128
+
+    random_latent_vectors = tf.random.normal(shape=(batch_size, latent_dim))
+
+    fake = generator(random_latent_vectors)
+
+    image_list = []
+
+    for i in range(5):
+        img = keras.preprocessing.image.array_to_img(fake[i])
+        
+        buffer = BytesIO()
+        
+        img.save(buffer, "PNG")
+        
+        # Encode as base64 and convert to string
+        img_bytes = base64.b64encode(buffer.getvalue()).decode('utf-8')  
+
+        image_list.append(img_bytes)
+
+    context = {'image': image_list}
+
+    return render(request, 'index.html', context)
 
 
 def getPhotos(request):
