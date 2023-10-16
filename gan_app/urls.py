0cb46246eff1cdfa1249ba680209d7ff7830441e
Fusion-Pie <piyushhatewar@gmail.com>
2023-10-16 23:37:32 +0530

    Modified Templates

--- gan_app/urls.py
+++ gan_app/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# Media images configuration
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('GAN_APP_1.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
